e865da3c428ef6d6bfe5d4812e46d6374db064e6
omerrklc <omerrklc0@gmail.com>
2026-09-01 16:08:04 +0300

    Enable FUTMAC forum

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -8,7 +8,7 @@
   enabled: true,
   leagueManagementEnabled: true,
   analyticsEnabled: true,
-  forumEnabled: false,
+  forumEnabled: true,
   url: 'https://pwmwymvnyfvrumlizbuw.supabase.co',
     publishableKey: 'sb_publishable_yOZqUUh7u78n7cTFetzWKQ_qn-Oa2mc',
     mediaBucket: 'futmac-media'
